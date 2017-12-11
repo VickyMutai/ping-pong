4879eda9ad8c66463dc14c9f99e22a79d214e05d
VickyMutai <vicky.mutai96@gmail.com>
2017-12-11 12:45:06 +0300

    add for each on user interface logic

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+var pingPonged=[];
 //business logic interface
 //user interface logic
 $(document).ready(function(){
   $("form#ping").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#number").val());
-    var result = pingPong(number);
-    $("#result ul").append('<li>'+comment+'</li>');
+    pingPonged.forEach(function(number){
+      $("#results").append("<li>"+inputNumber+"</li>");
+    });
   });
 });
