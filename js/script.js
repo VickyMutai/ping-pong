e604cb2c76a2d8acc21156c01bd9f7fe06e06885
VickyMutai <vicky.mutai96@gmail.com>
2017-12-11 13:09:10 +0300

    check if number is divisible by 15

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 var pingPonged=[];
 //business logic interface
+function pingPong(number){
+for (var num=1;num<=number; num++ ){
+  if (num %15 === 0){
+    pingPonged.push("pingpong");
+  }
+}
+}
 //user interface logic
 $(document).ready(function(){
   $("form#ping").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#number").val());
+    pingPong(number);
     pingPonged.forEach(function(number){
-      $("#results").append("<li>"+inputNumber+"</li>");
+      $("#results").append("<li>"+number+"</li>");
     });
   });
 });
