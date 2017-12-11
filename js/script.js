1d23dd5d15f2dac75113175492513d1b0c0c272a
VickyMutai <vicky.mutai96@gmail.com>
2017-12-11 12:16:42 +0300

    add code for output

--- js/script.js
+++ js/script.js
@@ -4,5 +4,7 @@ $(document).ready(function(){
   $("form#ping").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#number").val());
+    var result = pingPong(number);
+    $("#result ul").append('<li>'+comment+'</li>');
   });
 });
